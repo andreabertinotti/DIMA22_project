0dd59b21d6ef6d12dba2c6bdd580cd14b517b463
Andrea Bertinotti <71279139+andreabertinotti@users.noreply.github.com>
2023-08-20 00:02:04 +0200

    fix messaggio per profilo utente non completo

--- lib/Screen/menu.dart
+++ lib/Screen/menu.dart
@@ -42,21 +42,296 @@ class _MenuState extends State<Menu> {
             child: CircularProgressIndicator(),
           );
         }
+
+        //// HANDLE CASE IN WHICH USER STILL HASN'T COMPLETED PROFILE DATA
         if (!snapshot.hasData ||
             snapshot.data == null ||
             snapshot.data!.docs.isEmpty) {
-          return Center(
-            child: Text(
-                'You still have still haven\'t completed your profile! \n Go to profile page to complete it!.'),
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.orange[900],
+              toolbarHeight: 0,
+            ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 120,
+                  child: Stack(
+                    children: [
+                      Container(
+                        color: Colors.orange[900],
+                        width: double.infinity,
+                        height: 120,
+                      ),
+                      Positioned(
+                        top: 20,
+                        right: 15,
+                        child: CircleAvatar(
+                          // User image
+                          radius: 40,
+                          backgroundColor: Colors.orange,
+                          child: Center(
+                              child: Text(
+                            "!",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                      ),
+                      Positioned(
+                          left: 10,
+                          top: 45,
+                          child: Text('Your profile is not complete!',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold))),
+                      Positioned(
+                          left: 10,
+                          top: 65,
+                          child: Text('Complete it in the profile page',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 13))),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  padding: EdgeInsets.only(top: 30, left: 10, right: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Rules of the lockers service',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere cursus magna, at commodo elit volutpat a. Maecenas ante felis, tempus ut feugiat nec, vestibulum non elit. Ut euismod consequat nisl, sit amet aliquam magna consectetur eu. In aliquam venenatis mi, quis facilisis mauris tempus sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam sed augue eget mollis. Ut fringilla ligula non nunc luctus, sit amet rutrum sem tempus. Nullam sit amet mauris varius, suscipit ante vel, commodo neque. Integer sed justo a dolor scelerisque dignissim id et magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                        textAlign: TextAlign.justify,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(
+                              'Report a problem',
+                              style: TextStyle(color: Colors.orange),
+                            ),
+                            content: TextField(
+                              controller: descriptionController,
+                              minLines: 1,
+                              maxLines: 6,
+                              decoration: InputDecoration(
+                                hintText: "Description",
+                                //errorText: descriptionController.text.isNotEmpty ? null : "Problem description can't be empty",
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  //TODO: send problem report to db
+                                  Navigator.of(context).pop();
+                                  print(descriptionController.text);
+                                },
+                                child: Text(
+                                  'Submit',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.orange),
+                                ),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.orange),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                              side: BorderSide(color: Colors.orange)))),
+                  child: Text("Report a problem".toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      )),
+                )
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                authService.signOut(); // Handle "Logout" button press
+              },
+              backgroundColor: Colors.orange[900],
+              child: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
+            ),
           );
         }
 
+        //// HANDLE CASE IN WHICH USER STILL HASN'T COMPLETED PROFILE DATA (second case: no document in db for logged user)
         final docs = snapshot.data?.docs;
         if (docs == null || docs.isEmpty) {
-          return Center(
-            child: Text('No data available.'),
+          Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.orange[900],
+              toolbarHeight: 0,
+            ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 120,
+                  child: Stack(
+                    children: [
+                      Container(
+                        color: Colors.orange[900],
+                        width: double.infinity,
+                        height: 120,
+                      ),
+                      Positioned(
+                        top: 20,
+                        right: 15,
+                        child: CircleAvatar(
+                          // User image
+                          radius: 40,
+                          backgroundColor: Colors.orange,
+                          child: Center(
+                              child: Text(
+                            "!",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                      ),
+                      Positioned(
+                          left: 10,
+                          top: 45,
+                          child: Text('Your profile is not complete!',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold))),
+                      Positioned(
+                          left: 10,
+                          top: 65,
+                          child: Text('Complete it in the profile page',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 13))),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  padding: EdgeInsets.only(top: 30, left: 10, right: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Rules of the lockers service',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris posuere cursus magna, at commodo elit volutpat a. Maecenas ante felis, tempus ut feugiat nec, vestibulum non elit. Ut euismod consequat nisl, sit amet aliquam magna consectetur eu. In aliquam venenatis mi, quis facilisis mauris tempus sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam sed augue eget mollis. Ut fringilla ligula non nunc luctus, sit amet rutrum sem tempus. Nullam sit amet mauris varius, suscipit ante vel, commodo neque. Integer sed justo a dolor scelerisque dignissim id et magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                        textAlign: TextAlign.justify,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(
+                              'Report a problem',
+                              style: TextStyle(color: Colors.orange),
+                            ),
+                            content: TextField(
+                              controller: descriptionController,
+                              minLines: 1,
+                              maxLines: 6,
+                              decoration: InputDecoration(
+                                hintText: "Description",
+                                //errorText: descriptionController.text.isNotEmpty ? null : "Problem description can't be empty",
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  //TODO: send problem report to db
+                                  Navigator.of(context).pop();
+                                  print(descriptionController.text);
+                                },
+                                child: Text(
+                                  'Submit',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.orange),
+                                ),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.orange),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                              side: BorderSide(color: Colors.orange)))),
+                  child: Text("Report a problem".toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      )),
+                )
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                authService.signOut(); // Handle "Logout" button press
+              },
+              backgroundColor: Colors.orange[900],
+              child: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
+            ),
           );
         }
+
+        /////////////////////////////////////////////////////////
+        /// HANDLE CASE IN WHICH USER HAS ALREADY COMPLETED HIS PROFILE
         // Extract user data from the document snapshot
         var userData = snapshot.data!.docs[0];
 
