3079197d06c91d6d9c5c9c55c9821d800159cdc9
Andrea Bertinotti <71279139+andreabertinotti@users.noreply.github.com>
2023-08-25 17:54:55 +0200

    login test -work in progress

---
+++ test/WidgetTests/login_screen_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pro/Screen/login_screen.dart';
+
+void main() {
+  testWidgets('One Register button must be present',
+      (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+    final textFinder = find.textContaining("Register");
+
+    expect(textFinder, findsOneWidget);
+  });
+
+  testWidgets('One Login button must be present', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+    final textFinder = find.textContaining("Login");
+
+    expect(textFinder, findsOneWidget);
+  });
+}
+
+
+// flutter test test/WidgetTests/login_screen_test.dart
