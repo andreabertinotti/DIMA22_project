93c3ada98ffb433417b883253b2338d6155c00d7
Andrea Bertinotti <71279139+andreabertinotti@users.noreply.github.com>
2023-08-25 15:11:43 +0200

    test reserv list + fix reserv list (phone+ tablet)

--- lib/Screen/Reservations/reservations_list.dart
+++ lib/Screen/Reservations/reservations_list.dart
@@ -3,16 +3,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:pro/Models/user_model.dart';
 import 'package:pro/Screen/Reservations/reservations_history.dart';
-import 'package:pro/Services/auth_service.dart';
-import 'package:provider/provider.dart'; // package used to edit date format
 import 'package:pro/Screen/Reservations/reservation_add.dart';
 
 // A stateful widget representing the bookings page.
 class BookingsPage extends StatefulWidget {
-  const BookingsPage({super.key});
-
+  const BookingsPage({super.key, required this.uid});
+  final String uid;
   @override
   State<BookingsPage> createState() => _BookingsPageState();
 }
@@ -42,6 +39,7 @@ class CustomListItem extends StatelessWidget {
   final int duration;
   final String reservationId;
   final VoidCallback onDelete;
+
   //final bool notificationSet;
   //final int price;
   // final Widget lockerImage;
@@ -88,6 +86,7 @@ class CustomListItem extends StatelessWidget {
                         fontWeight: FontWeight.w600,
                       ),
                     ),
+
                     Text(
                       // used 'intl' package for date format
                       "Pick-up: ${DateFormat('dd/MM/yyyy, HH:mm').format(pickUp)}",
@@ -193,13 +192,13 @@ class _BookingsPageState extends State<BookingsPage> {
       GlobalKey<ScaffoldMessengerState>();
 
   void _deleteReservation(
-      String reservationId, User user, String locker, String cell) async {
+      String reservationId, String locker, String cell) async {
     try {
       await FirebaseFirestore.instance.runTransaction((transaction) async {
         // Delete the reservation document
         final reservationRef = FirebaseFirestore.instance
             .collection('users')
-            .doc(user.uid)
+            .doc(widget.uid)
             .collection('reservations')
             .doc(reservationId);
         transaction.delete(reservationRef);
@@ -256,12 +255,11 @@ class _BookingsPageState extends State<BookingsPage> {
   //  return reservations;
   //}
 
-  Stream<List<Map<String, dynamic>>> fetchReservationsLive(User user) {
+  Stream<List<Map<String, dynamic>>> fetchReservationsLive() {
     final DateTime currentTime = DateTime.now();
-
     return FirebaseFirestore.instance
         .collection('users')
-        .doc(user.uid)
+        .doc(widget.uid)
         .collection('reservations')
         .where('reservationEndDate',
             isGreaterThan: Timestamp.fromDate(currentTime))
@@ -278,140 +276,120 @@ class _BookingsPageState extends State<BookingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    final authService = Provider.of<AuthService>(context);
-    return StreamBuilder<User?>(
-      stream: authService.user,
-      builder: (_, AsyncSnapshot<User?> snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          final User? user = snapshot.data;
-          return ScaffoldMessenger(
-            key: _bookingMessengerKey,
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.orange,
-                foregroundColor: Colors.white,
-                title: const Text(
-                  'My Reservations',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ReservationsHistoryPage()));
-                    },
-                    icon: Icon(
-                      Icons.history,
-                      color: Colors.white,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  EditBooking(uid: snapshot.data!.uid)));
-                    },
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+    return ScaffoldMessenger(
+      key: _bookingMessengerKey,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          foregroundColor: Colors.white,
+          title: const Text(
+            'My Reservations',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ReservationsHistoryPage()));
+              },
+              icon: Icon(
+                Icons.history,
+                color: Colors.white,
               ),
-              body: StreamBuilder<List<Map<String, dynamic>>>(
-                stream: fetchReservationsLive(user!),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text("Error loading data."),
-                    );
-                  } else if (snapshot.data!.isEmpty) {
-                    return Center(
-                      child: Text("No booking found!",
-                          style: TextStyle(fontSize: 20)),
-                    );
-                  } else {
-                    return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        final String locker = snapshot.data![index]['locker'];
-                        final String cell = snapshot.data![index]['cell'];
-                        final DateTime dropOff = snapshot.data![index]
-                                ['reservationStartDate']
-                            .toDate();
-                        final DateTime pickUp = snapshot.data![index]
-                                ['reservationEndDate']
-                            .toDate();
-                        //final String baggageSize =
-                        //    snapshot.data![index]['baggageSize'];
-                        final int duration =
-                            snapshot.data![index]['reservationDuration'];
-                        final String reservationId =
-                            snapshot.data![index]['id'];
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => EditBooking(uid: widget.uid)));
+              },
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        body: StreamBuilder<List<Map<String, dynamic>>>(
+          stream: fetchReservationsLive(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("Error loading data."),
+              );
+            } else if (snapshot.data!.isEmpty) {
+              return Center(
+                child:
+                    Text("No booking found!", style: TextStyle(fontSize: 20)),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final String locker = snapshot.data![index]['locker'];
+                  final String cell = snapshot.data![index]['cell'];
+                  final DateTime dropOff =
+                      snapshot.data![index]['reservationStartDate'].toDate();
+                  final DateTime pickUp =
+                      snapshot.data![index]['reservationEndDate'].toDate();
+                  //final String baggageSize =
+                  //    snapshot.data![index]['baggageSize'];
+                  final int duration =
+                      snapshot.data![index]['reservationDuration'];
+                  final String reservationId = snapshot.data![index]['id'];
 
-                        // Create a CustomListItem using the data retrieved from Firestore
-                        return ExpansionTile(
-                          title: Text("Reservation @ $locker",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20)),
-                          subtitle: Text(
-                            "from ${DateFormat('dd/MM/yyyy').format(dropOff)}",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          textColor: Colors.orange,
-                          iconColor: Colors.orange,
-                          leading: CircleAvatar(
-                            backgroundColor: Colors.orange,
-                            child: Text(
-                              "${index + 1}",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                          children: <Widget>[
-                            CustomListItem(
-                              dropOff: dropOff,
-                              pickUp: pickUp,
-                              //baggageSize: baggageSize,
-                              duration: duration,
-                              cell: cell,
-                              locker: locker,
-                              reservationId: reservationId,
-                              onDelete: () => _deleteReservation(
-                                  reservationId,
-                                  user,
-                                  locker,
-                                  cell), // Pass a callback to delete
-                              //notificationSet: snapshot.data![index]['notificationSet'],
-                              //price: snapshot.data![index]['price'],
-                            )
-                          ],
-                        );
-                      },
-                    );
-                  }
+                  // Create a CustomListItem using the data retrieved from Firestore
+                  return ExpansionTile(
+                    title: Text("Reservation @ $locker",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                    subtitle: Text(
+                      "from ${DateFormat('dd/MM/yyyy').format(dropOff)}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    textColor: Colors.orange,
+                    iconColor: Colors.orange,
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.orange,
+                      child: Text(
+                        "${index + 1}",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                    children: <Widget>[
+                      CustomListItem(
+                        dropOff: dropOff,
+                        pickUp: pickUp,
+                        //baggageSize: baggageSize,
+                        duration: duration,
+                        cell: cell,
+                        locker: locker,
+                        reservationId: reservationId,
+                        onDelete: () => _deleteReservation(
+                            reservationId,
+                            //user,
+                            locker,
+                            cell), // Pass a callback to delete
+                        //notificationSet: snapshot.data![index]['notificationSet'],
+                        //price: snapshot.data![index]['price'],
+                      )
+                    ],
+                  );
                 },
-              ),
-            ),
-          );
-        } else {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
