19902ae30dc8afcd11c4a4ee966b3e6041775c45
Andrea Bertinotti <71279139+andreabertinotti@users.noreply.github.com>
2023-08-27 13:34:58 +0200

    register screen test

--- lib/Screen/register_screen.dart
+++ lib/Screen/register_screen.dart
@@ -16,112 +16,137 @@ class Register extends StatelessWidget {
     final authService = Provider.of<AuthService>(context);
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
+    //AuthService? authService = null;
+    //if (!Platform.environment.containsKey('FLUTTER_TEST')) {
+    //  authService = Provider.of<AuthService>(context);
+    //}
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        foregroundColor: Colors.white,
-        toolbarHeight: 0,
-      ),
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          foregroundColor: Colors.white,
+          toolbarHeight: 0,
+        ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 80, bottom: 25),
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(shape: BoxShape.circle, 
-                  image:  DecorationImage(
-                    image: AssetImage(
-                        'assets/images/circleAppLogo.png'),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Padding(
+              padding: EdgeInsets.only(top: 80, bottom: 25),
+              child: Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/circleAppLogo.png'),
                         fit: BoxFit.cover)),
-                ),
               ),
-              Text(
-                "Welcome to Milan Locker!", 
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+            ),
+            Text(
+              "Welcome to Milan Locker!",
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 35),
+              child: Text(
+                "Please register to start using the app",
+                style: TextStyle(fontSize: 16),
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 35),
-                child: Text("Please register to start using the app", style: TextStyle(fontSize: 16),),
-              ),
-              Center(
-                child: Form(
-                  key: formGlobalKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width > 500 
-                            ? MediaQuery.of(context).size.width * 0.4 
+            ),
+            Center(
+              child: Form(
+                key: formGlobalKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width > 500
+                            ? MediaQuery.of(context).size.width * 0.4
                             : MediaQuery.of(context).size.width * 0.75,
-                          child: TextFormField(
-                            validator: (value) => EmailFieldValidator.validate(value!),
-                            controller: emailController,
-                            cursorColor: Colors.orange,
-                            decoration: const InputDecoration(
-                             labelText: "Email", focusColor: Colors.orange, floatingLabelStyle: TextStyle(color: Colors.orange),
-                              border: OutlineInputBorder(), focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange))
-                            ),
-                          ),
+                        child: TextFormField(
+                          validator: (value) =>
+                              EmailFieldValidator.validate(value!),
+                          controller: emailController,
+                          cursorColor: Colors.orange,
+                          decoration: const InputDecoration(
+                              labelText: "Email",
+                              focusColor: Colors.orange,
+                              floatingLabelStyle:
+                                  TextStyle(color: Colors.orange),
+                              border: OutlineInputBorder(),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.orange))),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width > 500 
-                            ? MediaQuery.of(context).size.width * 0.4 
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width > 500
+                            ? MediaQuery.of(context).size.width * 0.4
                             : MediaQuery.of(context).size.width * 0.75,
-                          child: TextFormField(
-                            validator: (value) => PasswordFieldValidator.validate(value!),
-                            obscureText: true,
-                            cursorColor: Colors.orange,
-                            controller: passwordController,
-                            decoration: const InputDecoration(
-                              labelText: "Password", focusColor: Colors.orange, floatingLabelStyle: TextStyle(color: Colors.orange),
-                              border: OutlineInputBorder(), focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange))
-                            ),
-                          ),
+                        child: TextFormField(
+                          validator: (value) =>
+                              PasswordFieldValidator.validate(value!),
+                          obscureText: true,
+                          cursorColor: Colors.orange,
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                              labelText: "Password",
+                              focusColor: Colors.orange,
+                              floatingLabelStyle:
+                                  TextStyle(color: Colors.orange),
+                              border: OutlineInputBorder(),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.orange))),
                         ),
                       ),
-                      Container(
-                        padding: EdgeInsets.only(bottom: 5),
-                        width: MediaQuery.of(context).size.width > 500 
-                          ? MediaQuery.of(context).size.width * 0.2 
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 5),
+                      width: MediaQuery.of(context).size.width > 500
+                          ? MediaQuery.of(context).size.width * 0.2
                           : MediaQuery.of(context).size.width * 0.45,
-                        child: ElevatedButton(
+                      child: ElevatedButton(
                           onPressed: () async {
                             if (formGlobalKey.currentState!.validate()) {
                               authService!
                                   .createUserWithEmailAndPassword(
-                                      emailController.text, passwordController.text)
+                                      emailController.text,
+                                      passwordController.text)
                                   .then((usr) => {
                                         FirebaseFirestore.instance
                                             .collection('User')
                                             .doc(usr!.uid)
                                             .set({
-                                          "username": usr!.email!
-                                              .substring(0, usr.email!.indexOf('@')),
+                                          "username": usr!.email!.substring(
+                                              0, usr.email!.indexOf('@')),
                                           "email": usr.email,
                                         })
                                       });
                               Navigator.pop(context);
-                            }
-                            else{
+                            } else {
                               showDialog(
                                 context: context,
                                 builder: (BuildContext context) {
                                   return AlertDialog(
-                                    title: Text("Registration Error", style: TextStyle(color: Colors.orange),),
+                                    title: Text(
+                                      "Registration Error",
+                                      style: TextStyle(color: Colors.orange),
+                                    ),
                                     content: Text(
                                         "Please check the inserted values for email and password"),
                                     actions: <Widget>[
                                       TextButton(
-                                        child: Text("OK", style: TextStyle(color: Colors.orange),),
+                                        child: Text(
+                                          "OK",
+                                          style:
+                                              TextStyle(color: Colors.orange),
+                                        ),
                                         onPressed: () {
                                           Navigator.of(context).pop();
                                         },
@@ -133,36 +158,43 @@ class Register extends StatelessWidget {
                             }
                           },
                           style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18),
-                              side:BorderSide(color: Colors.orange)))),
-                          child: Text("Register", style: TextStyle(color: Colors.white, fontSize: 18),)),
-                      ),
-                      Padding(
+                              foregroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.white),
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.orange),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(18),
+                                      side: BorderSide(color: Colors.orange)))),
+                          child: Text(
+                            "Register",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          )),
+                    ),
+                    Padding(
                         padding: const EdgeInsets.only(bottom: 0),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text("Already a member?"),
                             TextButton(
-                              onPressed: () {
+                                onPressed: () {
                                   Navigator.pop(context);
-                              }, 
-                              child: const Text("Login", style: TextStyle(color: Colors.orange, fontWeight: FontWeight.w600),)
-                            ),
+                                },
+                                child: const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.orange,
+                                      fontWeight: FontWeight.w600),
+                                )),
                           ],
-                        )
-                      ),
-                    ],
-                  ),
+                        )),
+                  ],
                 ),
-              )
-            ]
-          ),
-        ) 
-      );
+              ),
+            )
+          ]),
+        ));
   }
 }
