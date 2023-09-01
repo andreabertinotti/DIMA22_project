0ba8e88a8afa42b5ca717f969896ee5ee98282e4
Andrea Bertinotti <71279139+andreabertinotti@users.noreply.github.com>
2023-09-01 11:18:23 +0200

    register screen tests enriched

--- test/WidgetTests/register_screen_test.dart
+++ test/WidgetTests/register_screen_test.dart
@@ -40,10 +40,80 @@ void main() {
       (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(MaterialApp(home: Register()));
-
+    expect(find.text('Already a member?'), findsOneWidget);
     expect(find.text('Login'), findsOneWidget);
   });
-}
 
+  testWidgets(
+      'User tries to register without filling the email and password fields',
+      (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(home: Register()));
+    await tester.tap(find.text('Register'));
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsNWidgets(1));
+    expect(find.text('Registration Error'), findsOneWidget);
+    expect(find.text('Please check the inserted values for email and password'),
+        findsOneWidget);
+
+    // close alert dialog
+    await tester.tap(find.text('OK'));
+
+    expect(find.text('Invalid Email'), findsOneWidget);
+    expect(find.text('Password can\'t be empty or shorter than 6 characters'),
+        findsOneWidget);
+  });
+
+  testWidgets('User tries to register with wronlgly fromatted email',
+      (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(home: Register()));
+
+    final emailField = find.widgetWithText(TextFormField, 'Email');
+    final passwordField = find.widgetWithText(TextFormField, 'Password');
+
+    // email with wrong formatting
+    await tester.enterText(emailField, 'wrong.email');
+    // in this case password is fine
+    await tester.enterText(passwordField, 'verystroNGpassWord229!');
+    await tester.tap(find.text('Register'));
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsNWidgets(1));
+    expect(find.text('Registration Error'), findsOneWidget);
+    expect(find.text('Please check the inserted values for email and password'),
+        findsOneWidget);
+
+    // close alert dialog
+    await tester.tap(find.text('OK'));
+
+    expect(find.text('Invalid Email'), findsOneWidget);
+  });
+
+  testWidgets('User tries to register with short password',
+      (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(home: Register()));
+
+    final emailField = find.widgetWithText(TextFormField, 'Email');
+    final passwordField = find.widgetWithText(TextFormField, 'Password');
+
+    // email is fine
+    await tester.enterText(emailField, 'fakeemail@test.it');
+    // in this case password is fine
+    await tester.enterText(passwordField, 'short');
+    await tester.tap(find.text('Register'));
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsNWidgets(1));
+    expect(find.text('Registration Error'), findsOneWidget);
+    expect(find.text('Please check the inserted values for email and password'),
+        findsOneWidget);
+
+    // close alert dialog
+    await tester.tap(find.text('OK'));
+
+    expect(find.text('Password can\'t be empty or shorter than 6 characters'),
+        findsOneWidget);
+  });
+}
 
 // flutter test test/WidgetTests/register_screen_test.dart
